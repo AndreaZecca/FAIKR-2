0a7a11e9655650b64e1473570bc341fbef016337
Andrea Zecca <andreazecca03@gmail.com>
2023-01-15 14:36:18 +0100

    Updated solutions

--- 2020 July 07.pl
+++ 2020 July 07.pl
@@ -3,6 +3,12 @@ how_many(C,N):-
     findall(P, clause(C,P), L),
     length(L,N).
 
+% another possible solution for how_many
+how_many2(C,N):-
+    bagof(B, C^clause(C,B), L),
+    length(L,N).
+    
+
 a(X):-b(X),c(X).
 a(X):- Y is X+1, b(Y), c(Y).
 a(X) :- Y is X-1, b(Y), c(Y).
--- 2021 June 28.pl
+++ 2021 June 28.pl
@@ -4,6 +4,10 @@ count(C, N):-
     findall(A, clause(C,A), L),
     length(L, N).
 
+% another possible solution for count 
+count2(C,N):-
+    bagof(B, C^clause(C,B),L),
+    length(L, N).
 
 p(a).
 p(b).
